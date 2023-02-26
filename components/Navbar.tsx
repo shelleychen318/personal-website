eb2809355c41ecfac22e32df79193b38c4abd777
Shelley Chen <shelleychen318@gmail.com>
2023-02-25 21:13:19 -0500

    basic styling to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,42 +1,34 @@
-// import React from "react";
-
 import Link from "next/link";
-
 import styles from "styles/Navbar.module.scss";
 
 export default function Navbar() {
-
-    const links = [
-      {
-        id: 1,
-        link: "Home",
-        href: "/",
-      },
-      {
-        id: 2,
-        link: "About",
-        href: "/about",
-      },
-      {
-        id: 3,
-        link: "Projects",
-        href: "/projects",
-      },
-    ];
+  const links = [
+    {
+      id: 1,
+      link: "Home",
+      href: "/",
+    },
+    {
+      id: 2,
+      link: "About",
+      href: "/about",
+    },
+    {
+      id: 3,
+      link: "Projects",
+      href: "/projects",
+    },
+  ];
 
   return (
-    <header>
-      <nav>
-        <Link href="/">SC</Link>
-        <ul >
-          {links.map(({ id, link, href }) => (
-            <li key={id}>
-              <Link href={href}>{link}</Link>
-            </li>
-          ))}
-        </ul>
+    <header className={styles.navbar}>
+      <Link href="/" className={styles.logo}>SC</Link>
+
+      <nav className={styles.links}>
+        {links.map(({ id, link, href }) => (
+          <Link key={id} href={href}>{link}</Link>
+        ))}
       </nav>
     </header>
   );
-  
 }
