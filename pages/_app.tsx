8cb471ca358cca582aa0a1aeff5249f2adba010c
Shelley Chen <shelleychen318@gmail.com>
2023-02-26 18:10:04 -0500

    finished home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Poppins } from "next/font/google";
 
-const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500"] });
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import Navbar from '../components/Navbar'
-import homeImg from "../assets/homeImg.png"
+import homeImg from "../public/assets/homeImg.png"
 
 export default function Home() {
   return (
@@ -14,11 +14,14 @@ export default function Home() {
 
       <Navbar />
 
-      <div>
-        <h2>Hi, I'm Shelley</h2>
-        <p>A developer who thrives in collaborative and creative spaces. Constantly growing.</p>
-        {/* <img src="homeImg" alt="me" width={150} height={150}/> */}
+      <div className={styles.container}>
+        <h1 className={styles.title}>Hi there! I'm Shelley.</h1>
+        <h3>
+          A developer who thrives in collaborative and creative spaces.
+          Constantly growing.
+        </h3>
+        <Image src={homeImg} alt="me" className={styles.image} />
       </div>
     </>
-  )
+  );
 }
