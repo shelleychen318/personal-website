50133a6f45879a16fb5a54ac4702c0951efb59a0
Shelley Chen <shelleychen318@gmail.com>
2023-02-26 19:59:53 -0500

    added mobile navbar ui

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { BiHome, BiGridAlt, BiPaperPlane } from "react-icons/bi";
 import styles from "styles/Navbar.module.scss";
 
 export default function Navbar() {
@@ -9,17 +10,20 @@ export default function Navbar() {
     {
       id: 1,
       link: "Home",
+      icon: <BiHome size={25} />,
       href: "/",
     },
     {
       id: 2,
-      link: "About",
-      href: "/about",
+      link: "Projects",
+      icon: <BiGridAlt size={25} />,
+      href: "/projects",
     },
     {
       id: 3,
-      link: "Projects",
-      href: "/projects",
+      link: "About",
+      icon: <BiPaperPlane size={25} />,
+      href: "/about",
     },
   ];
 
@@ -29,9 +33,10 @@ export default function Navbar() {
         SC
       </Link>
       <nav className={styles.links}>
-        {links.map(({ id, link, href }) => (
+        {links.map(({ id, link, icon, href }) => (
           <Link key={id} href={href} onClick={() => router.push(href)}>
-            {link}
+            <span className={styles.linksText}>{link}</span>
+            <span className={styles.icons}>{icon}</span>
           </Link>
         ))}
       </nav>
