0256d369f5be281277e1a06b5fec501e5561a173
Shelley Chen <shelleychen318@gmail.com>
2023-03-01 18:54:13 -0500

    added styling  to active link on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,7 @@ import styles from "styles/Navbar.module.scss";
 
 export default function Navbar() {
   const router = useRouter();
+  const currentLink = router.pathname;
 
   const links = [
     {
@@ -34,7 +35,12 @@ export default function Navbar() {
       </Link>
       <nav className={styles.links}>
         {links.map(({ id, link, icon, href }) => (
-          <Link key={id} href={href} onClick={() => router.push(href)}>
+          <Link
+            key={id}
+            href={href}
+            onClick={() => router.push(href)}
+            className={currentLink === href ? styles.activeLink : ""}
+          >
             <span className={styles.linksText}>{link}</span>
             <span className={styles.icons}>{icon}</span>
           </Link>
