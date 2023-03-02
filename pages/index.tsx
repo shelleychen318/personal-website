20efde456a0ba688195023f2cf760bdb668ddef9
Shelley Chen <shelleychen318@gmail.com>
2023-03-02 01:11:38 -0500

    added duck image to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { BiRocket } from "react-icons/bi";
 import Navbar from "../components/Navbar";
-import homeImg from "../public/assets/homeImg.png";
+import duck from "../public/assets/duck.png";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
@@ -16,17 +16,25 @@ export default function Home() {
 
       <Navbar />
 
-      <div className={styles.container2}>
-        <div className={styles.text}>
-          <h1>Hi there! I'm Shelley.</h1>
-          <h4>
-            A developer who thrives in collaborative and creative spaces.
-            Constantly growing.
-          </h4>
-          <Link href="/projects" className={`${styles.link} ${styles.bounce}`} >
-            <h4>View some of my work </h4>
-            <BiRocket size={25} className={styles.icon} />
-          </Link>
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <div className={styles.text}>
+            <h1>Hi there! I'm Shelley.</h1>
+            <h4>
+              A developer who thrives in collaborative and creative spaces.
+              Constantly growing.
+            </h4>
+            <Link
+              href="/projects"
+              className={`${styles.link} ${styles.bounce}`}
+            >
+              <h4>View some of my work </h4>
+              <BiRocket size={25} className={styles.icon} />
+            </Link>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src={duck} alt="duck" width="350" height="350" />
         </div>
       </div>
     </>
