6d9959d6ba952737a7683acfa45851196795dca9
Shelley Chen <shelleychen318@gmail.com>
2023-03-02 12:08:57 -0500

    cleaned up code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
-import { Playfair_Display, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600"] });
-
-const playfairDisplay = Playfair_Display({ subsets: ["latin"], weight: "500" })
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
