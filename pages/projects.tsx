a3aedd46b38154716c79da55459aacb5afa88f45
Shelley Chen <shelleychen318@gmail.com>
2023-05-03 19:11:03 -0400

    updated projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import classProfile from "../public/assets/classProfile.png";
 import coursify from "../public/assets/coursify.png";
 import aritzia from "../public/assets/aritziaApp.png";
 import movie from "../public/assets/movieDetailsApp.png";
-import blog from "../public/assets/blog.jpg";
 import expense from "../public/assets/expenseTrackerApp.png";
 import styles from "../styles/Projects.module.scss";
 
@@ -14,6 +14,14 @@ export default function Projects() {
   const projects = [
     {
       id: 1,
+      image: classProfile,
+      title: "SYDE '27 Class Profile",
+      description:
+        "The official class profile for the Systems Design Engineering 2027 cohort! Built using Next.js and MongoDB.",
+      link: "https://www.syde27classprofile.ca",
+    },
+    {
+      id: 2,
       image: coursify,
       title: "Coursify",
       description:
@@ -21,7 +29,7 @@ export default function Projects() {
       link: "https://github.com/shelleychen318/coursify",
     },
     {
-      id: 2,
+      id: 3,
       image: aritzia,
       title: "Aritzia App",
       description:
@@ -29,20 +37,12 @@ export default function Projects() {
       link: "https://github.com/shelleychen318/aritzia-app",
     },
     {
-      id: 3,
+      id: 4,
       image: movie,
       title: "Movie Details App",
       description:
         "Web application that displays details of any movie. Built using Next.js and IMDb API and styled using Tailwind CSS.",
       link: "https://movie-details-app.vercel.app/",
-    },
-    {
-      id: 4,
-      image: blog,
-      title: "Blog Website",
-      description:
-        "A personal blog website where I share my thoughts on various topics. Built using React, Next.js, and GraphQL.",
-      link: "https://javascript-blog.vercel.app/",
     },
     {
       id: 5,
